23bc486c9343646553258bf68ce52869340ed65d
Lauraponoran <lauraponoran7@gmail.com>
2026-04-15 13:54:21 +0200

    fix directory location

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ export const CONFIG = {
   MAP_STYLE: 'https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json',
   MAP_CENTER: [4.9041, 52.3676], // Amsterdam
   MAP_ZOOM: 13,
-  PMTILES_URL: '/marineterrein-commutes/trips.pmtiles',
-  DATA_URL: '/marineterrein-commutes/sensor_data' 
+  PMTILES_URL: './trips.pmtiles',        
+  DATA_URL: './sensor_data'  
 };
 
 // Make available globally
